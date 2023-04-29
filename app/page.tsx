5ce226c8391f22133419d974c13b9741df49fae3
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-04-29 07:21:35 +0100

    tramor ui added and home page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { Card, Divider, Text, Subtitle } from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Home</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-blue-950 to-blue-900 p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Home</Text>
+        <Subtitle className="text-xl text-center">
+          Power by OpenAI, Next.js 13.3 ,Taiwind CSS, Tremor 2.0 + More!
+        </Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-blue-950 to-blue-900">
+          {/* city picker */}
+          
+        </Card>
+      </Card>
+    </div>
   );
 }
